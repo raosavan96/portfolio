97608586fae0b1b30291b970492f1e9b24d5b369
= <=savanyadav377@gmail.com>
2024-06-18 12:56:55 -0800

    sawan

--- src/Component/Portfolio/Portfolio.js
+++ src/Component/Portfolio/Portfolio.js
@@ -241,7 +241,7 @@ function Portfolio() {
 
         <section className="my_projects">
           <div className="container">
-            <h1 className="my_projects-hadding">My Proj</h1>
+            <h1 className="my_projects-hadding">My Projects</h1>
           </div>
         </section>
       </div>
